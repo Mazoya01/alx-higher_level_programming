22c705fe0f10ea11a3bca6c11edacc65e01e75f0
Michael <gracemazoya77@gmail.com>
2023-01-06 01:04:35 -0800

    python import modules tasks

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
-    """Print all names defined by hidden_4 module."""
-    import hidden_4
+    from variable_load_5 import a
 
-    names = dir(hidden_4)
-    for name in names:
-        if name[:2] != "__":
-            print(name)
+    print(a)
